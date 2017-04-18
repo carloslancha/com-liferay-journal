6990931df0e40c7a749dc91c09c5e20036121a3a
Carlos Lancha <carlos.lancha@liferay.com>
2017-04-18 13:20:03 +0200

    LPS-71213 Implement select folder

--- journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
+++ journal-web/src/main/java/com/liferay/journal/web/internal/display/context/JournalDisplayContext.java
@@ -367,7 +367,7 @@ public class JournalDisplayContext {
 		return _folderId;
 	}
 
-	public String getFoldersJSON() throws Exception {
+	public JSONArray getFoldersJSONArray() throws Exception {
 		ThemeDisplay themeDisplay = (ThemeDisplay)_request.getAttribute(
 			WebKeys.THEME_DISPLAY);
 
@@ -383,7 +383,11 @@ public class JournalDisplayContext {
 		jsonObject.put(
 			"name", LanguageUtil.get(themeDisplay.getLocale(), "home"));
 
-		return jsonObject.toString();
+		JSONArray rootJsonArray = JSONFactoryUtil.createJSONArray();
+
+		rootJsonArray.put(jsonObject);
+
+		return rootJsonArray;
 	}
 
 	public String getFolderTitle() throws PortalException {
